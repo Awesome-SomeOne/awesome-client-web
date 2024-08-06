fd8fabf999d843b960de393810076cda8a865857
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-08-06 20:13:15 +0900

    💄 Style: shadow 업데이트
    Style: shadow 업데이트

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -65,6 +65,9 @@ export const Theme = {
     Label_L: typoCreator("14px", 600, "18px"),
     Label_M: typoCreator("12px", 600, "16px"),
     Label_S: typoCreator("10px", 600, "14px")
+  },
+  shadow: {
+    Neutral_Basic_L: "0px 0px 32px rgba(59, 62, 67, 0.08)"
   }
 };
 
