081d36fe5f0fb0af2d133f0bc81a9cc15dc6ef19
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-08-06 20:29:58 +0900

    ✨ Feat: bottomSheet 공통컴포넌트 추가
    children을 통해 내용물을 받는 bottomSheet 공통컴포넌트

---
+++ src/components/common/bottomSheet/styles.ts
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
+import { Theme } from "../../../styles/theme";
+
+export const BackDrop = styled(motion.div)`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  background-color: ${Theme.colors.Dim_Default};
+  z-index: 9;
+`;
+
+export const BottomSheetContainer = styled(motion.div)`
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: fit-content;
+  max-height: 80%;
+  background-color: ${Theme.colors.Fill_White};
+  border-radius: 16px 16px 0 0;
+  z-index: 10;
+`;
