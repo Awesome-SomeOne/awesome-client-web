ec1ef4fe0f641d302c8d71541c75fad92fa6c6ba
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-08-24 19:26:04 +0900

    feat: full page modal 공통 컴포넌트 추가

---
+++ src/components/common/fullPageModal/index.tsx
@@ -0,0 +1,35 @@
+import Clear from "@/assets/icons/Clear";
+import Appbar from "../header/Appbar";
+import Portal from "../portal";
+import * as S from "./styles";
+import { PropsWithChildren } from "react";
+
+interface FullPageModalProps extends PropsWithChildren {
+  isOpen?: boolean;
+  close: () => void;
+}
+
+const FullPageModal = ({ children, close }: FullPageModalProps) => {
+  return (
+    <Portal>
+      <S.FullPageModalContainer>
+        <Appbar
+          title=""
+          textAlign="center"
+          rightIcon1={
+            <button
+              onClick={() => {
+                close();
+              }}
+            >
+              <Clear />
+            </button>
+          }
+        />
+        <S.ContentWrapper>{children}</S.ContentWrapper>
+      </S.FullPageModalContainer>
+    </Portal>
+  );
+};
+
+export default FullPageModal;
