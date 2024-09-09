88102455cefbd4b33d99ef8ee96e757a39eac03e
seoyoon <5happy1004kr@naver.com>
2024-09-09 18:59:00 +0900

    ✨ Add: 홈, 내여행 페이지 라우터 추가

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -3,7 +3,9 @@ export const PATH = {
   MY_TRIP_LIST: "/my-trip-list", // 내 여행 목록 페이지
   MY_TRIP_RECORD: (tripId?: number) => `/my-trip-record/${tripId ?? ":tripId"}`, // 추억 기록하기 페이지
   MY_TRIP_RECORD_DETAIL: (tripId?: number) => `/my-trip-record/${tripId ?? ":tripId"}/detail`, // 추억 기록하기 상세 페이지
-  MAP: "/map" // 지도 페이지
+  MAP: "/map", // 지도 페이지
+  MY_TRIP_GENERATE: "/my-trip", // 내 여행 생성 페이지
+  MY_TRIP: (tripId?: number) => `/my-trip/${tripId ?? ":tripId"}` // 내 여행 조회 페이지
 };
 
 Object.freeze(PATH);
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,6 +6,9 @@ import MapPage from "@/pages/map/MapPage";
 import MyTripListPage from "@/pages/myTripList/MyTripListPage";
 import MyTripRecordPage from "@/pages/myTripRecord/MyTripRecordPage";
 import MyTripRecordDetailPage from "@/pages/myTripRecordDetail/MyTripRecordDetailPage";
+import MyTripPage from "@/pages/myTrip/MyTripPage";
+import PlanPage from "@/pages/myTrip/plan/PlanPage";
+import HomePage from "@/pages/home/HomePage";
 
 const AppRouter = () => {
   const router = createBrowserRouter([
@@ -16,6 +19,7 @@ const AppRouter = () => {
           id: "root",
           path: PATH.ROOT,
           children: [
+            { path: "", element: <HomePage /> },
             {
               path: PATH.MY_TRIP_LIST,
               element: <MyTripListPage />
@@ -31,6 +35,14 @@ const AppRouter = () => {
             {
               path: PATH.MAP,
               element: <MapPage />
+            },
+            {
+              path: PATH.MY_TRIP_GENERATE,
+              element: <MyTripPage />
+            },
+            {
+              path: PATH.MY_TRIP(),
+              element: <PlanPage />
             }
           ]
         }
