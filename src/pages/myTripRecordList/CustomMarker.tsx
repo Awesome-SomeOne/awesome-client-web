dc3b3b00f73695056ffba778cec108b79c75dff3
itsmesunky <itisp_06@naver.com>
2024-09-10 15:19:30 +0900

    ✨ Feat: CutsomMarker 컴포넌트 구현

---
+++ src/pages/myTripRecordList/CustomMarker.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { MapMarker } from "react-kakao-maps-sdk";
+
+type markerProps = {
+  position: { lat: number; lng: number };
+  image: {
+    src: string;
+    size: {
+      width: number;
+      height: number;
+    };
+  };
+  isSelected: boolean;
+  onClick: () => void;
+};
+
+export default function CustomMarker({ position, image, isSelected, onClick }: markerProps) {
+  const [marker, setMarker] = useState(image);
+
+  useEffect(() => {
+    if (isSelected) {
+      setMarker({
+        src: "/src/assets/images/ImageMarker/SelectedImageMarker.svg",
+        size: { width: 36, height: 50 }
+      });
+    } else {
+      setMarker(image);
+    }
+  }, [isSelected, image]);
+
+  return <MapMarker position={position} image={marker} onClick={onClick} />;
+}
