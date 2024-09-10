5c66a5512402608b6d215bced160223742913246
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-09-11 08:31:05 +0900

    feat: axios instance 및 queryClient 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
@@ -6,12 +7,27 @@ import AppRouter from "./routes/AppRouter";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { Theme } from "./styles/theme";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      throwOnError: true
+    },
+    mutations: {
+      retry: 1,
+      throwOnError: true
+    }
+  }
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <StrictMode>
-    <ThemeProvider theme={Theme}>
-      <GlobalStyle />
-      <AppRouter />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <AppRouter />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
