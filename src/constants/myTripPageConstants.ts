3c2fd3b032163e761d65c318191f1b52b2e5a69e
seoyoon <5happy1004kr@naver.com>
2024-09-19 14:23:33 +0900

    ✏️ Chore: 섬 리스트에 거제도 추가

--- src/constants/myTripPageConstants.ts
+++ src/constants/myTripPageConstants.ts
@@ -1,7 +1,7 @@
 export const ISLAND_LIST = [
   { id: 1, name: "울릉도" },
   { id: 2, name: "제주도" },
-  { id: 3, name: "울릉도2" },
+  { id: 3, name: "거제도" },
   { id: 4, name: "제주도2" },
   { id: 5, name: "울릉도3" },
   { id: 6, name: "제주도3" },
