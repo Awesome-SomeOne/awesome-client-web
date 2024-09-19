e81759dbb1af04df38bdab70a18c1aedd45bba21
seoyoon <5happy1004kr@naver.com>
2024-09-19 14:18:56 +0900

    💄 Chore: toast 스타일링 수정

--- src/components/myTrip/toast/styles.ts
+++ src/components/myTrip/toast/styles.ts
@@ -6,8 +6,9 @@ export const Toast = styled(motion.div)`
     position: fixed;
     top: 50px;
     left: 50%;
-    width: 128px;
+    width: max-content;
     height: 40px;
+    margin: 0 auto;
     ${Theme.typo.Body_L};
     background: ${Theme.colors.Fill_Primary_Lightest};
     color: ${Theme.colors.Label_Default};
