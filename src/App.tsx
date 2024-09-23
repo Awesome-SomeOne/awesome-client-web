bd5718fffa9071c21ee0b7bedabed82c15594f1d
seoyoon <5happy1004kr@naver.com>
2024-09-24 02:02:09 +0900

    🔥 Chore: QueryClient 중복 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import "./App.css";
 
 import { Outlet } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const queryClient = new QueryClient();
-
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        {/* <div>웹뷰 하이</div> */}
-        <Outlet />
-      </QueryClientProvider>
+      {/* <div>웹뷰 하이</div> */}
+      <Outlet />
     </>
   );
 }
