7d479ca0e9f109e0558d0b25ab9e45b01c732412
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-09-27 10:27:14 +0900

    feat: 맵 검색페이지 이동 연결

--- src/pages/map/MapPage.tsx
+++ src/pages/map/MapPage.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useNavigate } from "react-router-dom";
 
 import * as S from "./styles";
 import BottomNavBar from "@/components/common/bottomNavBar";
 import SearchBar from "@/components/common/searchBar";
 import MapFilterChips from "@/components/map/mapFilterChips";
 import MapListBottomSheet from "@/components/map/MapListBottomSheet";
+import { PATH } from "@/constants/path";
 import useOverlay from "@/hooks/useOverlay";
 
 const MapPage = () => {
   const { isOpen, close } = useOverlay();
   const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
   const handleSubmit = (value: string) => {
     setSearchQuery(value);
   };
@@ -19,12 +22,21 @@ const MapPage = () => {
     setSearchQuery(value);
   };
 
+  const handleInputClick = () => {
+    navigate(PATH.SEARCH);
+  };
+
   return (
     <S.MapPageWrapper>
       {/* <button onClick={() => toggle()}>sdfsdf</button> */}
       <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "100%" }}>
         <S.UpperBarContainer>
-          <SearchBar value={searchQuery} onValueChange={handleInputChange} onSearch={handleSubmit} />
+          <SearchBar
+            value={searchQuery}
+            onValueChange={handleInputChange}
+            onSearch={handleSubmit}
+            onClick={handleInputClick}
+          />
           <MapFilterChips />
         </S.UpperBarContainer>
         <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
