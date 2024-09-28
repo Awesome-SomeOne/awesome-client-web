cdba4aad14092b05fdc5990ea8df49c0e771de76
seoyoon <5happy1004kr@naver.com>
2024-09-29 00:33:53 +0900

    temp: 시작한 여행

--- src/components/home/MyTripToday/index.tsx
+++ src/components/home/MyTripToday/index.tsx
@@ -1,24 +1,41 @@
+import { useGetPlans } from "@/apis/myTrip/myTrip.queries";
 import { PATH } from "@/constants/path";
+import ErrorBoundary from "@/hooks/Errorboundary";
+import { Suspense } from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./styles";
 
-const MyTripToday = () => {
+const MyTripTodayContent = () => {
   const navigate = useNavigate();
+  const { data: plans = [] } = useGetPlans();
+
+  if (!plans.length) return <></>;
+
   return (
     <S.TripLayout>
-      <S.GradientBorder onClick={() => navigate(PATH.MY_TRIP(1))}>
+      <S.GradientBorder onClick={() => navigate(PATH.MY_TRIP(plans[0].planId))}>
         <S.Box>
           <S.Left>
             <S.Paragraph>여행 시작! 일정을 확인해봐요</S.Paragraph>
             <S.Info>
-              <S.Image src="" />
-              <S.Address>경상북도 울릉도</S.Address>
+              <S.Image src={plans[0].img_url} />
+              <S.Address>{plans[0].address}</S.Address>
             </S.Info>
           </S.Left>
           <img src="/icons/right.svg" />
         </S.Box>
       </S.GradientBorder>
     </S.TripLayout>
+  );
+};
+
+const MyTripToday = () => {
+  return (
+    <ErrorBoundary fallback={<>에러 발생</>}>
+      <Suspense fallback={<>로딩중...</>}>
+        <MyTripTodayContent />
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
