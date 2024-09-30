521efadd1a759882ea5d7f746643f8d93a961e13
itsmesunky <itisp_06@naver.com>
2024-09-30 16:06:48 +0900

    🐛 fix: axios 에러 > 토큰 인터셉터로 전달

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -3,8 +3,8 @@ import axios, { AxiosInstance } from "axios";
 const customAxios: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_REACT_APP_SERVER_HOST,
   headers: {
-    "Access-Control-Allow-Origin": "*",
-    Authorization: ("Bearer " + localStorage.getItem("token")) as string
+    "Access-Control-Allow-Origin": "*"
+    // Authorization: ("Bearer " + localStorage.getItem("token")) as string
   }
 });
 
@@ -12,10 +12,36 @@ const customAxiosMultipart: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_REACT_APP_SERVER_HOST,
   headers: {
     "Access-Control-Allow-Origin": "*",
-    Authorization: ("Bearer " + localStorage.getItem("token")) as string,
+    // Authorization: ("Bearer " + localStorage.getItem("token")) as string,
     "Content-Type": "multipart/form-data"
   }
 });
+
+customAxios.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+customAxiosMultipart.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 customAxios.interceptors.response.use(
   (response) => response,
