fd5f4b1497b2fd3fa4b5886ef90e7f27202762a2
itsmesunky <itisp_06@naver.com>
2024-10-02 10:06:39 +0900

    ✨ feat: 회원 탈퇴 API 추가

--- src/apis/myPage/myPage.apis.ts
+++ src/apis/myPage/myPage.apis.ts
@@ -1,6 +1,13 @@
 import { customAxios } from "../instance";
 
+// 회원 정보 가져오기
 export const getUserInfo = async () => {
   const response = await customAxios.get("/api/user/nickname");
+  return response.data;
+};
+
+// 회원 탈퇴
+export const deleteAccount = async () => {
+  const response = await customAxios.post("/api/user/unlink");
   return response.data;
 };
